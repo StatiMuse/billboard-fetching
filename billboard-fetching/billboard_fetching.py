1605a6dc9679d5b2bb79f18d6a79ad2e5b597b1f
Bao Vuong <vqbao@lv5.org>
2019-09-13 01:02:20 -0400

    stubs

--- billboard-fetching/billboard_fetching.py
+++ billboard-fetching/billboard_fetching.py
@@ -1,1 +1,16 @@
 import billboard
+
+from datetime import date, timedelta
+
+def fetchchart(chartname, date=date.today()):
+    print('fetching from %s at week of %s' % (chartname, date))
+
+if __name__ == '__main__':
+    interval = timedelta(days=7)
+    currentweek = date(1963, 8, 17)
+    chart = 'billboard-100'
+
+    while currentweek < date.today(): 
+        fetchchart(chart, currentweek)
+        currentweek += interval 
+
